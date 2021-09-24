c32e989f3f025146a0ace80f6e3e9dbfb909200d
Robin Kelsick II <robinkelsickii@gmail.com>
2021-09-24 16:30:49 -0400

    added full html and some js

---
+++ main.js
@@ -0,0 +1,12 @@
+var bigText = document.getElementById('bigText');
+var middleText = document.getElementById('middleText'); 
+var goReset = document.getElementById('goReset');
+var middleTextA = document.getElementById('middleTextA'); 
+var nextReveal = document.getElementById('nextReveal');  
+
+let currentPage = 0; 
+
+function go() {
+    currentPage ++;
+    
+}
